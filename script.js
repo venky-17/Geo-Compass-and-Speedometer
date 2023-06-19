f292d17f71cf866f55717b90aa22a16eb17cc29a
Venkatesh K <128626606+venky-17@users.noreply.github.com>
2023-06-19 10:48:00 +0530

    Update script.js
    added functionality to geo coordinates

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const arrow = document.querySelector('.arrow')
 const speed = document.querySelector('.speed-value')
+const longitude = document.querySelector('.longitude')
+const latitude = document.querySelector('.latitude')
 
 navigator.geolocation.watchPosition((data) =>{
     console.log(data);
     speed.textContent = Math.floor(data.coords.speed);
     arrow.style.transform = `rotate(${data.coords.heading}deg)`;
-   
+    longitude.textContent = data.coords.longitude;
+    latitude.textContent = data.coords.latitude;
 
 }, (err) =>{
     console.log(err);
